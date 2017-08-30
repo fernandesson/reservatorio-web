d4906c62c1963c8e9dfb220670485783af53df19
fernandesson <fernandes.son@gmail.com>
2017-08-30 13:48:35 -0300

    Função de donwload de dados de vazão em arquivo .csv

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^2$', views.index2, name='index2'),
     url(r'^3$', views.index3, name='index3'),
+    url(r'^download$', views.download, name='download'),
     url(r'^highchart_ex/$', views.highchart_ex, name='highchart_ex'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import csv
 
 from .models import Reservatorio, HistoricoVazoes, HistoricoEvaporacao
 
@@ -16,64 +17,27 @@ def index(request):
 		return render(request, 'app/index.html', {'query_reservatorio': query_reservatorio})
 
 
-def index2(request):
-	if request.method == 'POST':
-		id = request.POST.get('name_list')
+def download(request):
+	""" Gera um .csv com os dados do reservatorio selecionado
+	"""
+	if request.method == 'POST':							# Verifica se houve requisição
+		id = request.POST.get('name_list')					# Pega o reservatório selecionado.
 	else:
-		id = 1
+		id = 1												# Id padrão para abertura da página.
+	
+	query_reservatorio = Reservatorio.objects.filter(id=id)			# Pega o reservatório da id selecionada.
+	query_vazao = HistoricoVazoes.objects.filter(reservatorio=id)	# Pega o reservatório da id selecionada.
 
-	query_reservatorio_all = Reservatorio.objects.all()
-	query_vazao = HistoricoVazoes.objects.filter(reservatorio=id)
-	query_reservatorio = Reservatorio.objects.filter(id=id)
-	name = query_reservatorio[0].nome
-
-	vaz_mensal = list(range(12))
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="vazao.csv"'
+	
+	writer = csv.writer(response)
+	writer.writerow([query_reservatorio[0].nome])
+	writer.writerow(["Data", 'Vazao'])
 	for q in query_vazao:
-		if q.vazao != -999:
-			if q.data.month == 1:
-				vaz_mensal[0] += q.vazao
-			if q.data.month == 2:
-				vaz_mensal[1] += q.vazao
-			if q.data.month == 3:
-				vaz_mensal[2] += q.vazao
-			if q.data.month == 4:
-				vaz_mensal[3] += q.vazao
-			if q.data.month == 5:
-				vaz_mensal[4] += q.vazao
-			if q.data.month == 6:
-				vaz_mensal[5] += q.vazao
-			if q.data.month == 7:
-				vaz_mensal[6] += q.vazao
-			if q.data.month == 8:
-				vaz_mensal[7] += q.vazao
-			if q.data.month == 9:
-				vaz_mensal[8] += q.vazao
-			if q.data.month == 10:
-				vaz_mensal[9] += q.vazao
-			if q.data.month == 11:
-				vaz_mensal[10] += q.vazao
-			if q.data.month == 12:
-				vaz_mensal[11] += q.vazao
-
-	for i in range(12):
-		vaz_mensal[i] = vaz_mensal[i]/100
-
-	series_1 = [{								# Gráfico vazão mensal.
-		"name": name,
-		"data": vaz_mensal
-	}]
-
-	vaz_anual = []								# Lista para armazenar as vazões.
-	for x in query_vazao:						# Atribui o query a lista vazão.
-		if x.vazao != -999:						# Verifica se o dado é válido.
-			vaz_anual.append(x.vazao)			# Atribui a lista.
-
-	series_2 = [{								# Grafico_2 Vazao Anual.
-		"name": name,
-		"data": vaz_anual
-	}]
-
-	return render(request, 'app/highchart_example.html', {'series_1': series_1, 'series_2': series_2, 'query_reservatorio_all': query_reservatorio_all})
+		writer.writerow([q.data, q.vazao])
+		
+	return response
 
 
 def index3(request):
@@ -85,8 +49,8 @@ def index3(request):
 	else:
 		id = 1												# Id padrão para abertura da página.
 
-	query_reservatorio_all = Reservatorio.objects.all()		# Pega todos os reservatórios.
-	query_reservatorio = Reservatorio.objects.filter(id=id)	# Pega o reservatório da id selecionada.
+	query_reservatorio_all = Reservatorio.objects.all()				# Pega todos os reservatórios.
+	query_reservatorio = Reservatorio.objects.filter(id=id)			# Pega o reservatório da id selecionada.
 	
 	name = query_reservatorio[0].nome						# Pega o nome do reservatório.
 
