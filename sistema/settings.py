f56c51fbc17d05b0c6ccd662a3478929e22af137
fernandesson <fernandes.son@gmail.com>
2017-09-13 14:22:35 -0300

    Adição do Bootstrap com modificações do template

--- sistema/settings.py
+++ sistema/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f)c8z#a^q9o9!&ytao3+!@1s2!9vm&1m1z)#8_6tw65ricrz31'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.101.73.65']
 
 
 # Application definition
